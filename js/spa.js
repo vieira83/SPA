d78abdcee711d6fb6c0154da3a46a2c2ce97b66b
vladimir <vieira83@gmail.com>
2015-05-14 16:59:41 -0400

    added spa2.js

---
+++ js/spa.js
@@ -0,0 +1,23 @@
+/*
+ * spa.js
+ * Root namespace module
+*/
+
+/*jslint   brwoser:true, continue:true,
+  devel : true, indent : 2 maxerr : 50,
+  newcap : true, nomen : true, plusplus : true,
+  regexp : true, sloppy : true, vars : true,
+  white : true
+*/
+
+/* global $, spa */ 
+
+var spa = (function(){
+	var initModule = function($container){
+		$container.hmtl('<h1 style="display:inline-block margin:25px;"></h1>'
+				+'hello world!'
+			+'</h1>'
+		);	
+	};
+	return {initModule: initModule};
+}());
