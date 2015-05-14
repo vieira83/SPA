c03cedab115978a86ded5519136b641c76ccc08b
vladimir <vieira83@gmail.com>
2015-05-14 11:06:31 -0400

    Initial project setup

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="utf-8" />
+		<title> Backbone.js App | Tutorial</title>
+
+		<!-- Google web fonts -->
+		<link href="http://fonts.googleapis.com/css?family=PT+Sans:400,700" rel='stylesheet' />
+
+		<!-- The main CSS file -->
+		<link href="css/style.css" rel="stylesheet" />
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+	</head>
+
+	<body>
+		<div id="container">		
+			<h1>Boston PHP - JQuery Season 5</h1>
+			<div id="menu">
+				<ul id="">
+					
+				</ul>				
+			</div>
+				<!-- The services will be inserted here -->
+			<p id="">
+				 <span></span>
+			</p>
+
+			<input type="submit" id="order" value="Submit" />
+		</div>
+			
+		<!-- JavaScript Includes -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min.js"></script>
+	</body>
+</html>
